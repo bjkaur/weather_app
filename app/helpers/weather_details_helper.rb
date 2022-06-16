18dff33b2fea79072a93e75d0998d8a7e7dd0e34
Kaur <bjit_kaur@hotmail.com>
2022-06-16 13:57:22 +0100

    weather helper to get Hot, Warm, Cold

--- app/helpers/weather_details_helper.rb
+++ app/helpers/weather_details_helper.rb
@@ -1,2 +1,11 @@
 module WeatherDetailsHelper
+    def temperature
+        if @temperature._to_i <= 15
+            "Weather is Cold."
+        elsif @temperature.to_i >= 16
+            "Weather is Warm."
+        else @temperature.to_i > 17
+            "Weather is Hot"
+        end
+    end
 end
