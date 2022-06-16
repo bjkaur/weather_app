35969315807fcd021d0280bf6ce25d1cede2e09c
Kaur <bjit_kaur@hotmail.com>
2022-06-16 13:58:05 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  #get 'home/index'
+  # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#index"
+  
+  # Weather Search and Weather results routes
+  get 'search', to: 'home#search'
+
+  # Path for action in pages controller calling 
+  # ApiController to get weather data
+  get '/weather_search', to: 'weather_details#weather_search'
 end
