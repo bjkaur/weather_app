0b977c334c4f037ed8e22003a6825ff83ff7c688
Kaur <bjit_kaur@hotmail.com>
2022-06-17 01:44:17 +0100

    Helper method to get Hot, Warm and Cold weather.

--- app/helpers/weather_details_helper.rb
+++ app/helpers/weather_details_helper.rb
@@ -1,10 +1,10 @@
 module WeatherDetailsHelper
     def temperature
-        if @temperature._to_i <= 15
+        if @temperature.to_i <= 15
             "Weather is Cold."
-        elsif @temperature.to_i >= 16
+        elsif @temperature.to_i >= 16 && @temperature.to_i <= 20
             "Weather is Warm."
-        else @temperature.to_i > 17
+        else @temperature.to_i > 20 && @temperature.to_i <= 35
             "Weather is Hot"
         end
     end
