1cac96a29aef7a77f08729baffb233cadd31724f
Kaur <bjit_kaur@hotmail.com>
2022-06-17 14:18:14 +0100

    weather controller test file

--- test/controllers/weather_details_controller_test.rb
+++ test/controllers/weather_details_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class WeatherDetailsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Can see weather search details" do 
+    get "/"
+    assert_select "h1", "WealtherDetails#weather_search"
+  end
+
+  test "Parses JSON details of weather" do
+    postcode_uri = WealtherDetail.postcode_uri(postcode: "LE1")
+    @weather = JSON.parse WeatherDetail.new(postcode_uri).api_response
+    get @temperature
+    expect(response).to be_successful
+    expect(response.body).to include("Test parsing JSON")
+  end
 end
