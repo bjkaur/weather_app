ba70742a3514c3913564ec82cb12792b81663d2e
Kaur <bjit_kaur@hotmail.com>
2022-06-17 14:15:24 +0100

    Fetching data from API and displaying on weather search form

--- app/controllers/weather_details_controller.rb
+++ app/controllers/weather_details_controller.rb
@@ -4,18 +4,30 @@ class WeatherDetailsController < ApplicationController
     # Data displays on weather_search form
     def weather_search
         postcode_uri = WeatherDetail.postcode_uri(params[:postcode])
+        # JSON.parse takes the query response body and turns it into a hash
         @weather = JSON.parse WeatherDetail.new(postcode_uri).api_response
 
         # Saves temperature in degree celsius for the search input
-        if @weather.keys.include? 'current'
-            @temperature = @weather['current']['temp_c']
+        #byebug
+
+        #if @weather.keys.include? 'current'
+        #    @temperature = @weather['current']['temp_c']
+        
+        # For JSON e.g.
+        # "forecast": {
+        #    "forecastday": [
+        #        {
+        #            "date": "2022-06-17",
+        #            "date_epoch": 1655424000,
+        #            "day": {
+        #                "maxtemp_c": 25.1,
+        
+        if @weather.keys.include? 'forecast'
+            @temperature = @weather['forecast']['forecastday']['date'.to_i]['date_epoch']['day'.to_i]['maxtemp_c'.to_i]
         else 
             redirect_to search_path, alert: @weather.to_s
         end
-        
-        #@forecast = @weather['forecast']
-        #@forecastday = @weather['forecastday']
-        #@forecast.byebug
+
     end
 
 end
