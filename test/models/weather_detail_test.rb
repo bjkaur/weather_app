27c1401447f27d4dcb98b6bb2aff58da62cab525
Kaur <bjit_kaur@hotmail.com>
2022-06-17 14:18:45 +0100

    weather model test fiel

--- test/models/weather_detail_test.rb
+++ test/models/weather_detail_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class WeatherDetailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "GET request" do 
+    api.get('api/uri')
+    expect(api.status).to eq (200)
+    expect(api.message).to eq('OK')
+  end
+
+  if "POST request" do 
+    api.post('/api/uri', {"website": "abcdefg"})
+    expect(api.status).to eq(201)
+  end
 end
